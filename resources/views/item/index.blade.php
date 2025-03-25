d93c419b7b7cbb7c9b34b39cd110a2cfc40adc8f
namorin10 <namorin10x@gmail.com>
2025-03-25 16:41:23 +0900

    変更

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -59,7 +59,48 @@
                                     <td>{{ $item->preference }}</td>
                                     <td>{{ $item->progress }}</td>
                                     <td>
-                                        <button class="detail-button">企業詳細</button>
+                                        <div class="col-3">
+                                            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal1">企業詳細</button>
+                                                    <div class="modal fade" id="modal1" tabindex="-1" area-hidden="true">
+                                                        <div class="modal-dialog modal-xl">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h2 class="modal-title">企業の詳細</h2>
+                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <div class="row">
+                                                                        <div class="col-4">企業名</div>
+                                                                        <div class="col-8">日本郵便</div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <div class="col-4">志望度</div>
+                                                                        <div class="col-8">2</div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <div class="col-4">良いところ</div>
+                                                                        <div class="col-8">ない</div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <div class="col-4">懸念点</div>
+                                                                        <div class="col-8">経営不振</div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <div class="col-4">メモ</div>
+                                                                        <div class="col-8">あんまり行きたくない</div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <div class="col-4">URL</div>
+                                                                        <div class="col-8">日本郵便のURL</div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <a href="/recruit/edit" class="btn btn-primary">編集</a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                        </div>
                                     </td>
                                     <td>
                                         <form action="/items/delete/{{$item->id}}" method="post">
